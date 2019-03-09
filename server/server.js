75d4af211f0cf5206fcba8878871f5df78d1c6ef
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-08 17:14:18 -0800

    Implement API for app

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 // SERVER FILE
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('../database/index.js')
+
 
 const app = express();
-const { getProduct } = require('../database/index.js');
-const { addToCart } = require('../database/index.js');
-const { itemsInCart } = require('../database/index.js');
 
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 app.use(express.static(`${__dirname}/../client/dist`));
 
 
 app.get('/api/product/:id', (req, res) => {
   const idToSearch = req.params.id;
-  getProduct(idToSearch, (err, data) => {
+  db.getProduct(idToSearch, (err, data) => {
     if (err) {
       res.status(400).send();
       return;
@@ -25,24 +28,141 @@ app.get('/api/product/:id', (req, res) => {
     res.status(200).send(data);
   });
 });
+app.get('/api/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.getUser(userId, (err, user) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
 
-app.post('/api/cart/:userId', (req, res) => {
+app.post('/api/cart/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.addToCart(productId, userId, (err, user) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
+app.post('/api/ideaList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.addToIdeaList(productId, userId, (err, user) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
+app.post('/api/shoppingList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.addToShoppingList(productId, userId, (err, user) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
+app.post('/api/wishList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.addToWishList(productId, userId, (err, user) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
+app.post('/api/User/:userId', (req, res) => {
   // create a new user for the website
   // = insert into postgres.
+  // sample query for the future Insert into Users (userid, name, addresses, shopping_list_privacy, wish_list_privacy, idea_list_privacy) values (30000000, 'Luke Singer', ARRAY['Winterfell'], false, false, false);
 });
 
-app.put('/api/cart/:productId', (req, res) => {
-  // we will be updating the quantity of the product in the db.
-  // update into cart
-  // update into a list
-  // updating delivery address
-  // update an address.
-  // update privacy settings.
+app.put('/api/shoppingList/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.updateShoppingListPrivacy(userId, (err, user) => {
+    if(err) {
+      res.status(400).send();
+    } else {
+      res.status(200).send(user);
+    }
+  })
+});
+app.put('/api/wishList/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.updateWishListPrivacy(userId, (err, user) => {
+    if(err) {
+      res.status(400).send();
+    } else {
+      res.status(200).send(user);
+    }
+  })
+});
+app.put('/api/ideaList/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.updateIdeaListPrivacy(userId, (err, user) => {
+    if(err) {
+      res.status(400).send();
+    } else {
+      res.status(200).send(user);
+    }
+  })
 });
 
-app.delete('/api/cart/:userId', (req, res) => {
-  // will allow for someone to delete an item out of their cart.
-  // remove user
+app.delete('/api/cart/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.removeFromCart(productId, userId, (err, product) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(product);
+  });
+});
+app.delete('/api/shoppingList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.removeFromShoppingList(productId, userId, (err, product) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(product);
+  });
+});
+app.delete('/api/wishList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.removeFromWishList(productId, userId, (err, product) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(product);
+  });
+});
+app.delete('/api/ideaList/:productId/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  db.removeFromIdeaList(productId, userId, (err, product) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(product);
+  });
 });
 
 
