5ac7b86ee26072d308d7c11e8da553e6d84118e6
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-12 13:00:00 -0700

    Prepare service for ec2

--- database/config.example.js
+++ database/config.example.js
@@ -1,7 +1,7 @@
 module.exports = {
   user: 'postgres',
-  password: 'Birthday30%',
+  password: 'postgres',
   host: 'ec2-54-86-54-106.compute-1.amazonaws',
-  database: 'postgres',
+  database: 'service_cart',
   port: 5432,
 }
