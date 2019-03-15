d3ccade276cb2aa2938624400abb47adc9d5f118
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-14 17:18:52 -0700

    Update to allow for just a product in url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(`${__dirname}/../client/dist`));
 
 
-app.get('/api/:productid/:userid', (req, res) => {
+app.get('/api/:productid/', (req, res) => {
   const idToSearch = req.params.productid;
   console.log(req.params)
   db.getProduct(idToSearch, (err, data) => {
