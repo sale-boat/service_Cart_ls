bd7d29a477a921d59bc9644cbef493c6647f3c10
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-03-15 15:35:25 -0700

    Implement new config security in preparation for multiple databases

--- database/config.example.js
+++ database/config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: 'postgres',
-  password: 'postgres',
+  user: '',
+  password: '',
   host: 'ec2-34-239-109-138.compute-1.amazonaws.com',
   database: 'service_cart',
   port: 5432,
--- database/index.js
+++ database/index.js
@@ -3,15 +3,9 @@ const {
 } = require('pg');
 
 
-const config = require('./config.example.js');
+const config = require('./config.js');
 
-const pool = new Pool({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'ec2-34-239-109-138.compute-1.amazonaws.com',
-  database: 'service_cart',
-  port: 5432,
-});
+const pool = new Pool(config);
 // const pool = new Pool({
 //   host: 'localhost',
 //   database: 'service_cart',
